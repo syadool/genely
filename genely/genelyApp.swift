da3cadd0543d3af007ff0e7afd1f333c0bbdddfe
syadool <takerumorimoto0417@gmail.com>
2023-01-24 09:05:15 +0900

    Initial Commit

---
+++ genely/genelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  genelyApp.swift
+//  genely
+//
+//  Created by 森元豪琉 on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct genelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
